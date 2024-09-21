76580cfd8b4058c6cb3c47fb0522918789d40aa7
Валентин Папков <valentinpapkov77@gmail.com>
2024-09-21 17:26:30 +0300

    ver 4

--- Dota2DraftHelper/ViewModels/MainWindowViewModel.cs
+++ Dota2DraftHelper/ViewModels/MainWindowViewModel.cs
@@ -268,11 +268,14 @@ public partial class MainWindowViewModel : ObservableObject
 
         if (!IsAllHeroes)
         {
-            counterPicks = counterPicks.Where(x => ownPicks.Contains(x.PickId) && enemyIds.Contains(x.CounterPickId) && x.WinRateDate == DateTime.Now.Date).ToList();
+            counterPicks = counterPicks.Where(x => ownPicks.Contains(x.PickId) && 
+            enemyIds.Contains(x.CounterPickId) && x.WinRateDate == DateTime.Now.Date &&
+                            x.CounterPickId != x.PickId).ToList();
         }
         else
         {
-            counterPicks = counterPicks.Where(x => enemyIds.Contains(x.CounterPickId) && x.WinRateDate == DateTime.Now.Date).ToList();
+            counterPicks = counterPicks.Where(x => enemyIds.Contains(x.CounterPickId) && x.WinRateDate == DateTime.Now.Date &&
+                            x.CounterPickId != x.PickId).ToList();
         }
 
         return counterPicks;
